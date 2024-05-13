08d92ca1c80d9b431e6c7d3d319136a6847a38c7
ispong <ispong@outlook.com>
2024-05-13 22:38:35 +0800

    优化项目slogen

--- lucene-yun-frontend/src/views/workflow/work-item/return-data.vue
+++ lucene-yun-frontend/src/views/workflow/work-item/return-data.vue
@@ -4,7 +4,7 @@
  * @LastEditTime: 2023-06-18 16:13:37
  * @LastEditors: fanciNate
  * @Description: In User Settings Edit
- * @FilePath: /spark-yun/spark-yun-website/src/views/workflow/work-item/return-data.vue
+ * @FilePath: /lucene-yun/lucene-yun-website/src/views/workflow/work-item/return-data.vue
 -->
 <template>
   <BlockTable
